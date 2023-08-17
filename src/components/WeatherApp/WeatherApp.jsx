281be0087f08a8433bb037c32787e5e8da43c52e
Fjean <fjean0522@gmail.com>
2023-08-17 17:30:45 -0400

    Add humidity & wind speed elements & icons.

--- src/components/WeatherApp/WeatherApp.jsx
+++ src/components/WeatherApp/WeatherApp.jsx
@@ -28,7 +28,19 @@ const WeatherApp = () => {
 
       <div className="data-container">
         <div className="element">
-          
+          <img src={humidity_icon} alt="" className="icon" />
+          <div className="data">
+            <div className="humidity-percentage">64%</div>
+            <div className="text">Humidity</div>
+          </div>
+        </div>
+
+        <div className="element">
+          <img src={wind_icon} alt="" className="icon" />
+          <div className="data">
+            <div className="humidity-percentage">18 km/h</div>
+            <div className="text">Wind Speed</div>
+          </div>
         </div>
       </div>
     </div>
